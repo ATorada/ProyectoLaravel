af35cba33fdd610a31fc7d2d6a5b6c6ce998bc25
Sabiin <sabiintg@gmail.com>
2023-02-01 02:49:34 +0100

    FEAT - Event Migration

---
+++ database/migrations/2023_02_01_012659_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('description');
+            $table->boolean('visibility')->default(0);
+            $table->date('date');
+            $table->time('hour');
+            $table->string('location', 100);
+            $table->string('tags', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
