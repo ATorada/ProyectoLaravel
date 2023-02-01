01bd73caba01c451a732da246dc8f4151aa58cb8
Sabiin <sabiintg@gmail.com>
2023-02-01 07:51:18 +0100

    FEAT - Header Design

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<nav>
+    <div class="logo">
+        <img src="{{ asset('img/logo.svg') }}" alt="Logo">
+    </div>
+    <div class="menu">
+        <ul>
+            <li><a href="{{ route('index') }}">Inicio</a></li>
+            <li><a href="{{ route('users.index') }}">Miembros</a></li>
+            <li><a href="{{ route('events.index') }}">Eventos</a></li>
+            <li><a href="{{ route('messages.create') }}">Contacto</a></li>
+            <li><a href="{{ route('where') }}">Donde estamos</a></li>
+        </ul>
+    </div>
+</nav>
