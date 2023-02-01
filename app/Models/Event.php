e8226eea4ac85cbdccccffb9d29ba8a232328637
Sabiin <sabiintg@gmail.com>
2023-02-01 03:25:26 +0100

    FEAT - Many To Many User_Event

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
     use HasFactory;
 }
---
+++ database/migrations/2023_02_01_022427_create_user_event_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_event', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('event_id')->constrained();
+            $table->unique(['user_id', 'event_id'], 'user_event_unique');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_event');
+    }
+};
