dd65d58a5ec3ee40a9d6e00a4e09cedd701b5e7c
Sabiin <sabiintg@gmail.com>
2023-02-01 04:07:13 +0100

    FIX - Imports for the Controllers on web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
