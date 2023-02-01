ba72a4b3ecc560f143f4c748b362eed3e70f05b0
Sabiin <sabiintg@gmail.com>
2023-02-01 07:51:30 +0100

    FEAT - Footer Design

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,17 @@
+<footer>
+    <div class="footer">
+        <div class="text">
+            <p>Asociación</p>
+            <p>Desarrollado por Ángel</p>
+        </div>
+        <div class="links">
+            <ul>
+                <li><a href="{{-- {{ route('cookies') }} --}}">Política de Cookies</a></li>
+                <li><a href="{{-- {{ route('cookies') }} --}}">Configuración de Cookies</a></li>
+                <li><a href="{{-- {{ route('privacy') }} --}}">Política de Privacidad</a></li>
+                <li><a href="{{-- {{ route('terms') }} --}}">Términos y condiciones de uso</a></li>
+                <li><a href="{{ route('messages.create') }}">Contacto</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
