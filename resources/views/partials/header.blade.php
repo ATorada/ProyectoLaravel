792a7cedaa8b20e0a93bebfcf2ed1e61e616108d
Sabiin <sabiintg@gmail.com>
2023-02-08 01:00:56 +0100

    FEAT - Header now filters admin users

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,6 +9,16 @@
             <li><a href="{{ route('events.index') }}">Eventos</a></li>
             <li><a href="{{ route('messages.create') }}">Contacto</a></li>
             <li><a href="{{ route('where') }}">Dónde estamos</a></li>
+            @auth
+                <li><a href="{{ route('logout') }}">Cerrar sesión</a></li>
+            @else
+                <li><a href="{{ route('login') }}">Iniciar sesión</a></li>
+                <li><a href="{{ route('register') }}">Registrarse</a></li>
+            @endauth
+            @if (auth()->check() && auth()->user()->role == 'admin')
+                <li><a href="{{ route('events.create') }}">Crear evento</a></li>
+                <li><a href="{{ route('messages.index') }}">Mensajes</a></li>
+            @endif
         </ul>
     </div>
 </nav>
