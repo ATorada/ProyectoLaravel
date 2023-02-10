52a61cb6a7a835e815e097f8de4a61969c7fee46
ATorada <sabiintg@gmail.com>
2023-02-10 18:20:52 +0100

    FEAT - Error page done

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('title', 'Error')
+
+@section('content')
+    <div class="content">
+        <h1>Error 404</h1>
+        <p>La página que estás buscando no existe.</p>
+    </div>
+@endsection
