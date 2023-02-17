86482c4db36b34d431c2d44e4c09c126e8127221
Sabiin <sabiintg@gmail.com>
2023-02-17 02:59:39 +0100

    FEAT - 500 Error Page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('title', 'Error')
+
+@section('content')
+    <div class="content">
+        <h1>Error 500</h1>
+        <p>La página no está disponible.</p>
+    </div>
+@endsection
